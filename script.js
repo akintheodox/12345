1891105fc12aa9df1b6c4f98e0b2362cb723d262
akintheodox <akintheodox@gmail.com>
2025-05-18 19:13:23 +0100

    webp bg

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const img = new Image();
-    img.src = './images/gameback.jpg';
+    img.src = './images/gameback.webp';
     
     img.onload = () => {
         document.body.classList.add('loaded');
